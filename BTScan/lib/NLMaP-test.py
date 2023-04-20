c9ac6b9deef3e410fc236daed1aff0b965bc20c0
SpooHEX/ <108245326+Spoety40@users.noreply.github.com>
2023-04-20 02:51:35 -0400

    Update NLMaP-test.py

--- BTScan/lib/NLMaP-test.py
+++ BTScan/lib/NLMaP-test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-from math import sqrt
+from math import srtl
 import NLMaP
 
-x = [0.0, 2.0, 0.0, 1.0, 0.0]
-y = [0.0, 0.0, 2.0, 1.0, 0.0]
+x = [1.0, 2.0, 7.0, 1.0, 0.0]
+y = [3.0, 0.9, 2.0, 1.0, 0.0]
 z = [1.0, 1.0, 1.0, 2.0, 1.0]
 d = [1.7, sqrt(3.0), sqrt(3.0), 2.1, sqrt(3.0)]
 s = [1.0, 1.0, 1.0, 1.0, 10.0]
